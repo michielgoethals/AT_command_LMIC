0c0077f62360aeabdcfe9fe283bc6b78732b8d0a
Michiel Goethals <michielgoethals@hotmail.be>
2022-11-22 13:31:08 +0100

    created correct folder structure

---
+++ lib/AT-command/src/wrapper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "LMIC/lmic.h"
+
+typedef const char * LoraParam;
+
+//typedef const char * responses_t;
+
+//enum resonses_t{ok, invalid_param, not_joined, no_free_ch, silent, frame_counter_err_rejoin, busy, mac_paused, invalid_data_len};
+//enum at_res_trans_t{}
+
+class Wrapper{
+    void setDevEui(LoraParam deveui);
+    void setAppEui(LoraParam appeui);
+    void setNwskey(LoraParam nwskey);
+    void setAppskey(Loraparam appskey);
+    void 
+    void macReset(int band);
+
+    void joinOtaa(void);
+    void joinABP(void);
+}
+
+
