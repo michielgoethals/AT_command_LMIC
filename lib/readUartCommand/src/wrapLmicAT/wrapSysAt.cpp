71d90896541c5b14cdf0c5e82183e2e6c83bd5f3
Michiel Goethals <michielgoethals@hotmail.be>
2023-03-30 20:55:22 +0200

    fixed keys restore with sys reset command

--- lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
+++ lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
@@ -2,11 +2,31 @@
 
 void WrapSysAt::sysReset(WrapMacAt *macWrapper){
     macWrapper->macReset(*(u2_t*)EEPROM_START_ADDR_BAND);
-    macWrapper->setDevEui((char*)EEPROM_START_ADDR_DEVEUI);
-    macWrapper->setAppEui((char*)EEPROM_START_ADDR_APPEUI);
-    macWrapper->setAppKey((char*)EEPROM_START_ADDR_APPKEY);
-    macWrapper->setNwkskey((char*)EEPROM_START_ADDR_NWKSKEY);
-    macWrapper->setAppsKey((char*)EEPROM_START_ADDR_APPSKEY);
-    macWrapper->setNwkskey((char*)EEPROM_START_ADDR_NWKSKEY);
-    macWrapper->setDevAddr((char*)EEPROM_START_ADDR_DEVADDR);
+
+    char deveui[LORA_EUI_SIZE*2];
+    char appeui[LORA_EUI_SIZE*2];
+
+    for(u1_t i = 0; i < LORA_EUI_SIZE; i++){
+        sprintf(deveui+i*2, "%02X", *((char*)EEPROM_START_ADDR_DEVEUI+i));
+        sprintf(appeui+i*2, "%02X", *((char*)EEPROM_START_ADDR_APPEUI+i));
+    }
+    
+    macWrapper->setDevEui(deveui);
+    macWrapper->setAppEui(appeui);
+
+    char appkey[LORA_KEY_SIZE*2];
+    char nwkskey[LORA_KEY_SIZE*2];
+    char appskey[LORA_KEY_SIZE*2];
+
+    for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
+        sprintf(appkey+i*2, "%02X", *((char*)EEPROM_START_ADDR_APPKEY+i));
+        sprintf(nwkskey+i*2, "%02X", *((char*)EEPROM_START_ADDR_NWKSKEY+i));
+        sprintf(appskey+i*2, "%02X", *((char*)EEPROM_START_ADDR_APPSKEY+i));
+    }
+
+    macWrapper->setAppKey(appkey);
+    macWrapper->setNwkskey(nwkskey);
+    macWrapper->setAppsKey(appskey);
+
+    //TO DO SET CHANNELS
 }
