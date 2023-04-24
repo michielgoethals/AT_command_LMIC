c22f552e0d6343e4618043baeff6ac86e9ca1b25
Michiel Goethals <michielgoethals@hotmail.be>
2023-04-24 18:12:54 +0200

    added functions to cpp

---
+++ lib/readUartCommand/src/wrapLmicAT/wrapRadioAt.cpp
@@ -0,0 +1,146 @@
+#include "wrapRadioAt.h"
+
+// General methods
+void WrapRadioAt::rx(u2_t windowSize){
+
+}
+
+void WrapRadioAt::tx(char* data){
+
+}
+
+void WrapRadioAt::cw(char* state){
+
+}
+
+// Set methods
+void WrapRadioAt::setBt(char* gfBT){
+
+}
+
+void WrapRadioAt::setMod(char* mode){
+
+}
+
+void WrapRadioAt::setFreq(u4_t freq){
+
+}
+
+void WrapRadioAt::setPwr(s1_t pwrout){
+
+}
+
+void WrapRadioAt::setSf(char* spreadingFactor){
+
+}
+
+void WrapRadioAt::setAfcBw(float autoFreqBand){
+
+}
+
+void WrapRadioAt::setRxBw(float rxBandWidth){
+
+}
+
+void WrapRadioAt::setBitRate(u2_t fskBitRate){
+
+}
+
+void WrapRadioAt::setFdev(u2_t freqDev){
+
+}
+
+void WrapRadioAt::setPrLen(u2_t preamble){
+
+}
+
+void WrapRadioAt::setCrc(char* crcHeader){
+
+}
+
+void WrapRadioAt::setIqi(char* iqInvert){
+
+}
+
+void WrapRadioAt::setCr(char* codingRate){
+
+}
+
+void WrapRadioAt::setWdt(u4_t watchDog){
+
+}
+
+void WrapRadioAt::setSync(char* syncWord){
+
+}
+
+void WrapRadioAt::setBw(u2_t bandWidth){
+
+}
+
+
+// Get methods
+
+void WrapRadioAt::getBt(){
+
+}
+
+void WrapRadioAt::getMod(){
+
+}
+
+void WrapRadioAt::getFreq(){
+
+}
+
+void WrapRadioAt::getPwr(){
+
+}
+
+void WrapRadioAt::getSf(){
+
+}
+
+void WrapRadioAt::getAfcBw(){
+
+}
+
+void WrapRadioAt::getRxBw(){
+
+}
+
+void WrapRadioAt::getBitRate(){
+
+}
+
+void WrapRadioAt::getFdev(){
+
+}
+
+void WrapRadioAt::getPrLen(){
+
+}
+
+void WrapRadioAt::getCrc(){
+
+}
+
+void WrapRadioAt::getIqi(){
+
+}
+
+void WrapRadioAt::getCr(){
+
+}
+
+void WrapRadioAt::getWdt(){
+
+}
+
+void WrapRadioAt::getBw(){
+
+}
+
+void WrapRadioAt::getSnr(){
+    
+}
