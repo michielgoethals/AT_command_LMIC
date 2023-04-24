6d6180fca299f9f63284f9676da347b2d2bb6946
Michiel Goethals <michielgoethals@hotmail.be>
2023-04-24 18:29:13 +0200

    added all sys functions to class

--- lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
+++ lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
@@ -1,21 +1,29 @@
 #include "wrapSysAt.h"
 
-void WrapSysAt::reset(WrapMacAt *macWrapper){
-    macWrapper->reset(*(u2_t*)EEPROM_START_ADDR_BAND);
+//General
+String WrapSysAt::sleep(){
+    response = "ok";
+
+    return response;
+
+}
+
+String WrapSysAt::reset(WrapMacAt *macWrapper){
+    response = macWrapper->reset(*(u2_t*)EEPROM_START_ADDR_BAND);
 
     //restore deveui
     char deveui[LORA_EUI_SIZE*2];
     for(u1_t i = 0; i < LORA_EUI_SIZE; i++){
         sprintf(deveui+i*2, "%02X", *((char*)EEPROM_START_ADDR_DEVEUI+i));
     }
-    macWrapper->setDevEui(deveui);
+    response = macWrapper->setDevEui(deveui);
 
     //restore appeui
     char appeui[LORA_EUI_SIZE*2];
     for (u1_t i = 0; i < LORA_EUI_SIZE; i++){
         sprintf(appeui+i*2, "%02X", *((char*)EEPROM_START_ADDR_APPEUI+i));
     }
-    macWrapper->setAppEui(appeui);
+    response = macWrapper->setAppEui(appeui);
 
 
     //restore appkey
@@ -23,32 +31,68 @@ void WrapSysAt::reset(WrapMacAt *macWrapper){
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(appkey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_APPKEY+i)));    
     }
-    macWrapper->setAppKey(appkey);
+    response = macWrapper->setAppKey(appkey);
 
     //restore nwkskey
     char nwkskey[LORA_KEY_SIZE*2];
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(nwkskey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_NWKSKEY+i)));        
     }
-    macWrapper->setNwkskey(nwkskey);
+    response = macWrapper->setNwkskey(nwkskey);
 
     //restore appskey
     char appskey[LORA_KEY_SIZE*2];
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(appskey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_APPSKEY+i)));       
     }
-    macWrapper->setAppsKey(appskey);
+    response = macWrapper->setAppsKey(appskey);
 
     //restore devaddr
     char devaddr[LORA_ADDR_SIZE*2];
     for(u1_t i = 0; i < LORA_ADDR_SIZE; i++){
         sprintf(devaddr+i*2, "%02X", *((char*)EEPROM_START_ADDR_DEVADDR-i-1+LORA_ADDR_SIZE));
     }
-    macWrapper->setDevAddr(devaddr);
+    response = macWrapper->setDevAddr(devaddr);
 
     //TO DO SET CHANNELS
+
+    response = "ok";
+
+    return  response;
 }
 
-void WrapSysAt::sleep(){
+void WrapSysAt::eraseFW(){
+
+}
+
+String WrapSysAt::factoryRESET(){
+
+}
+
+
+//Set commands
+String WrapSysAt::setNvm(char address, char data){
+
+}
+
+String WrapSysAt::setPinDig(char pinName, u1_t pinState){
+
+}
+
+
+//Get commands
+String WrapSysAt::getVer(){
+
+}
+
+String WrapSysAt::getNvm(char address){
+
+}
+
+String WrapSysAt::getVdd(){
+
+}
+
+String WrapSysAt::getHweui(){
 
 }
--- lib/readUartCommand/src/wrapLmicAT/wrapSysAt.h
+++ lib/readUartCommand/src/wrapLmicAT/wrapSysAt.h
@@ -6,6 +6,22 @@ class WrapSysAt{
 private:
 
 public:
-    void reset(WrapMacAt *macWrapper);
-    void sleep();
+    //General
+    String sleep();
+    String reset(WrapMacAt *macWrapper);
+    void eraseFW();
+    String factoryRESET();
+
+    //Set commands
+    String setNvm(char address, char data);
+    String setPinDig(char pinName, u1_t pinState);
+
+    //Get commands
+    String getVer();
+    String getNvm(char address);
+    String getVdd();
+    String getHweui();
+
+private:
+    String response;
 };
