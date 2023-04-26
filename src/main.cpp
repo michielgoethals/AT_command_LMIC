5e5ffb4d7e3c0fae4c816d812d0e164169531834
Michiel Goethals <michielgoethals@hotmail.be>
2023-04-26 21:28:34 +0200

    added callbacks to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,43 @@
 
 #define BAUDRATE 19200
 
-char * command = nullptr;
+osjob_t initjob;
+osjob_t readAt;
 ReadUartCommand reader; 
 
+char * command = nullptr;
+
+static void initfunc(osjob_t* j){
+  //initializes serial communication and mac parameters
+  reader.begin();
+  //init done
+}
+
+static void readAtfunc(osjob_t* j){
+  //read command from serial port
+  command = reader.getCommand();
+  //parse command
+  reader.parseCommand(command);
+}
+
+
 void setup(){
-  reader.begin(BAUDRATE);
+  //initialize run-time environment
+  os_init();
+  //
+  os_setCallback(&initjob, initfunc);
 }
 
 void loop(){
-  command = reader.getCommand();
-  if (strcmp(command,"")!=0){
-    reader.parseCommand(command);
+  //schedule a read command if there is data available
+  if(Serial.available()>0){
+    //give time to fill buffer with full command and then read it
+    //since we dont know the length of the command
+    delay(500);
+    os_setCallback(&readAt, readAtfunc);
   }
-
-  delay(500);
-
-  delete[] command;
+  //execute scheduled jobs and events
+  os_runloop_once();
 }
+
+
