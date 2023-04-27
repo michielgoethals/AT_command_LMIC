adceac75cc06c03230d6b5ab17ef7194aee46e08
Michiel Goethals <michielgoethals@hotmail.be>
2023-04-27 20:44:58 +0200

    adde implementation

--- lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
+++ lib/readUartCommand/src/wrapLmicAT/wrapSysAt.cpp
@@ -9,21 +9,22 @@ String WrapSysAt::sleep(){
 }
 
 String WrapSysAt::reset(WrapMacAt *macWrapper){
-    response = macWrapper->reset(*(u2_t*)EEPROM_START_ADDR_BAND);
+    //restore band
+    macWrapper->reset(*(u2_t*)EEPROM_START_ADDR_BAND);
 
     //restore deveui
     char deveui[LORA_EUI_SIZE*2];
     for(u1_t i = 0; i < LORA_EUI_SIZE; i++){
         sprintf(deveui+i*2, "%02X", *((char*)EEPROM_START_ADDR_DEVEUI+i));
     }
-    response = macWrapper->setDevEui(deveui);
+    macWrapper->setDevEui(deveui);
 
     //restore appeui
     char appeui[LORA_EUI_SIZE*2];
     for (u1_t i = 0; i < LORA_EUI_SIZE; i++){
         sprintf(appeui+i*2, "%02X", *((char*)EEPROM_START_ADDR_APPEUI+i));
     }
-    response = macWrapper->setAppEui(appeui);
+    macWrapper->setAppEui(appeui);
 
 
     //restore appkey
@@ -31,34 +32,32 @@ String WrapSysAt::reset(WrapMacAt *macWrapper){
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(appkey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_APPKEY+i)));    
     }
-    response = macWrapper->setAppKey(appkey);
+    macWrapper->setAppKey(appkey);
 
     //restore nwkskey
     char nwkskey[LORA_KEY_SIZE*2];
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(nwkskey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_NWKSKEY+i)));        
     }
-    response = macWrapper->setNwkskey(nwkskey);
+    macWrapper->setNwkskey(nwkskey);
 
     //restore appskey
     char appskey[LORA_KEY_SIZE*2];
     for(u1_t i = 0; i < LORA_KEY_SIZE; i++){
         sprintf(appskey+i*2, "%02X", *((char*)(EEPROM_START_ADDR_APPSKEY+i)));       
     }
-    response = macWrapper->setAppsKey(appskey);
+    macWrapper->setAppsKey(appskey);
 
     //restore devaddr
     char devaddr[LORA_ADDR_SIZE*2];
     for(u1_t i = 0; i < LORA_ADDR_SIZE; i++){
         sprintf(devaddr+i*2, "%02X", *((char*)EEPROM_START_ADDR_DEVADDR-i-1+LORA_ADDR_SIZE));
     }
-    response = macWrapper->setDevAddr(devaddr);
+    macWrapper->setDevAddr(devaddr);
 
     //TO DO SET CHANNELS
 
-    response = "ok";
-
-    return  response;
+    return  "ok";
 }
 
 void WrapSysAt::eraseFW(){
@@ -71,12 +70,41 @@ String WrapSysAt::factoryRESET(){
 
 
 //Set commands
-String WrapSysAt::setNvm(char address, char data){
+String WrapSysAt::setNvm(char* address, char* data){
+    u2_t _address = strtol(address, NULL, 16);
+    u2_t _data = strtol(data, NULL, 16);
+    
+    if(_address < 0x300 || _address > 0x3FF){
+        response = "invalid_param";
+    }else if(_data < 0x00 || _data > 0xFF){
+        response = "invalid_param";
+    }else{
+        HAL_FLASHEx_DATAEEPROM_Unlock();
+        HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_BYTE, EEPROM_START_ADDR_BAND+_address, _data);
+        HAL_FLASHEx_DATAEEPROM_Lock();
 
+        response = "ok";
+    }
+    
+    return response;
 }
 
-String WrapSysAt::setPinDig(char pinName, u1_t pinState){
+String WrapSysAt::setPinDig(char* pinName, u1_t pinState){
+    if(pinState != 0 && pinState != 1){
+        response = "invalid_param";
+    }else{
+        for (u1_t i = 0; i < MAX_GPIO_PINS; i++){
+            if(String(pinName) == pinNameList[i]){
+                //pinMode(pinList[i], OUTPUT);
+                //digitalWrite(pinList[i], pinState);
+                response = "ok";
+            }else{
+                response = "invalid_param";
+            }
+        }
+    } 
 
+    return response; 
 }
 
 
@@ -93,6 +121,9 @@ String WrapSysAt::getVdd(){
 
 }
 
+//get hwei of radio module
 String WrapSysAt::getHweui(){
+    //u1_t hweui;
+    //hal_spi_read(0x80, &hweui, 8);
 
 }
--- lib/readUartCommand/src/wrapLmicAT/wrapSysAt.h
+++ lib/readUartCommand/src/wrapLmicAT/wrapSysAt.h
@@ -2,26 +2,31 @@
 
 #include "wrapMacAt.h"
 
+#define MAX_GPIO_PINS 18
+
 class WrapSysAt{
-private:
 
-public:
-    //General
-    String sleep();
-    String reset(WrapMacAt *macWrapper);
-    void eraseFW();
-    String factoryRESET();
+    public:
+        //General
+        String sleep();
+        String reset(WrapMacAt *macWrapper);
+        void eraseFW();
+        String factoryRESET();
 
-    //Set commands
-    String setNvm(char address, char data);
-    String setPinDig(char pinName, u1_t pinState);
+        //Set commands
+        String setNvm(char* address, char* data);
+        String setPinDig(char* pinName, u1_t pinState);
 
-    //Get commands
-    String getVer();
-    String getNvm(char address);
-    String getVdd();
-    String getHweui();
+        //Get commands
+        String getVer();
+        String getNvm(char address);
+        String getVdd();
+        String getHweui();
 
-private:
-    String response;
+    private:
+        String response;
+        String pinNameList[MAX_GPIO_PINS] = {"gpio0", "gpio1", "gpio2", "gpio3", "gpio4", "gpio5", "gpio6", "gpio7", "gpio8", "gpio9", 
+        "gpio10", "gpio11", "gpio12", "gpio13", "uart_cts", "uart_rts", "test0", "test1"};
+        int pinList[MAX_GPIO_PINS]; //TO DO: link pinNameList to pinList
+
 };
