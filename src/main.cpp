0dc558358bdc298cd7928fd6595931404b5d6bf4
Michiel Goethals <michielgoethals@hotmail.be>
2023-05-14 10:11:38 +0200

    added uart and rtc init functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "readUartCommand.h"
 
 #define BAUDRATE 19200
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_RX_Pin GPIO_PIN_3
 
 osjob_t initjob;
 osjob_t readAt;
 
+RTC_HandleTypeDef hrtc; 
 ReadUartCommand reader; 
 
 char * command = nullptr;
@@ -15,14 +18,24 @@ extern "C" void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
+  /* Enable Power Control clock */
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
   /** Configure the main internal regulator output voltage
   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
@@ -44,14 +57,65 @@ extern "C" void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_RTC;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+extern "C" void GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_USART2_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF4_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+extern "C" void RTC_Init(void)
+{
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+
+  __HAL_RCC_RTC_ENABLE();
+
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* RTC interrupt Init */
+  HAL_NVIC_SetPriority(RTC_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(RTC_IRQn);
 }
 
 static void initfunc(osjob_t* j){
    
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  //Reset of all peripherals, Initializes the Flash interface and the Systick.
   HAL_Init();
-  /* Configure the system clock */
+  //Configure the system clock
   SystemClock_Config();
+  //init Uart pins and enable clock
+  GPIO_Init();
+  //init RTC
+  RTC_Init();
   //initializes serial communication and mac parameters
   reader.begin();
   //init done
@@ -72,13 +136,11 @@ void setup(){
 }
 
 void loop(){
-  //schedule a read command if there is data available
-  if(Serial.available()>0){
-    //give time to fill buffer with full command and then read it
-    //since we dont know the length of the command
-    delay(100);
-    os_setCallback(&readAt, readAtfunc);
-  }
+  //schedule a read command
+  //give time to fill buffer with full command and then read it
+  //since we dont know the length of the command
+  os_setCallback(&readAt, readAtfunc);
+  HAL_Delay(100);
 
   //execute scheduled jobs and events
   os_runloop_once();
